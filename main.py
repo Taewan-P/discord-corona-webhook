6b2b04a7c14863c0b070fb72cc06b0c5e30e9ad6
Taewan-P <swimingtw@gmail.com>
2020-02-29 00:10:44 +0900

    Update webhook algorithm to not repeat itself

--- main.py
+++ main.py
@@ -5,12 +5,12 @@ from bs4 import BeautifulSoup
 from urllib.request import Request, urlopen
 from urllib import parse
 
-WAIT_TIME = 600
 WEBHOOK_ADDRESS="<YOUR DISCORD WEBHOOK ADDRESS>"
 
 def parse_info():
   req = Request("http://ncov.mohw.go.kr/index_main.jsp")
   res = urlopen(req)
+  time.sleep(3)
 
   bs = BeautifulSoup(res, "html.parser")
   status = [i.text for i in bs.findAll("a", attrs={"class": "num"})]
@@ -23,39 +23,56 @@ def parse_info():
 
   return status
 
+def send_result(stat):
+  try:
+    text = "확진환자수 : " + stat[0] + "\n" + "확진환자 격리해제수 : " + stat[1] + "\n" + "사망자수 : " + stat[2]
+  except IndexError as e:
+    return "ERROR"
+
+  url = WEBHOOK_ADDRESS
+  headers = {"Content-Type": 'application/json'}
+  data = {"embeds": [{"title": "## 한국 코로나 바이러스 현황 ##", "description": text, "color": 14366005}]}
+  r = requests.post(url, data=json.dumps(data), headers=headers)
+
 
 def main():
-  ticker = threading.Event()
-  temp = ""
-  while not ticker.wait(WAIT_TIME):
+  file = open("stats.txt", "r+")
+  a = parse_info()
+  laststat = file.readline()
+  if laststat == "\n":
+    laststat = []
+  else:
+    laststat = ast.literal_eval(laststat)
+    laststat = [i.strip() for i in laststat]
+
+  if (len(a) != 3):
+    # Parse Error, Trying one more time.
     a = parse_info()
-    time.sleep(60)
 
-    if (len(a) != 3):
-      a = temp
+  if (laststat == []):
+    # Initial
+    send_result(a)
+    file.close()
+    file = open("stats.txt", "w+")
+    file.write(str(a))
+    file.close()
+    return "INITIAL"
 
-    # Initialize
-    if (temp == ""):
-      temp = a
-      # Send request to discord then pass
-      text = "확진환자수 : " + temp[0] + "\n" + "확진환자 격리해제수 : " + temp[1] + "\n" + "사망자수 : " + temp[2]
-      url = WEBHOOK_ADDRESS
-      headers = {"Content-Type": 'application/json'}
-      data = {"embeds": [{"title": "## 한국 코로나 바이러스 현황 ##", "description": text, "color": 14366005}]}
-      r = requests.post(url, data=json.dumps(data), headers=headers)
-      pass
-
-
-    if (a == temp):
-      print("Same Status")
-      pass
+  else:
+    # Regular
+    if (a == laststat):
+      # Same Status
+      file.close()
+      return "PASS"
     else:
-      temp = a
-      url = WEBHOOK_ADDRESS
-      text = "확진환자수 : " + temp[0] + "\n" + "확진환자 격리해제수 : " + temp[1] + "\n" + "사망자수 : " + temp[2]
-      data = {"embeds": [{"title": "## 한국 코로나 바이러스 현황 ##", "description": text, "color": 14366005}]}
-      # Send request to discord
-      r = requests.post(url, data=json.dumps(data), headers=headers)
+      # Update Status
+      send_result(a)
+      file.close()
+      file = open("stats.txt", "w+")
+      file.write(str(a))
+      file.close()
+      return "UPDATED"
+  
+  file.close()
 
-
-main()
+print(main())
